e86d68f68dd56796bfcf116bde795c99b5169827
Jong MIn <unib335@naver.com>
2023-05-30 15:37:30 +0900

    Image, Card, 상태

--- app/src/main/java/com/example/jetpack_compose_example/MainActivity.kt
+++ app/src/main/java/com/example/jetpack_compose_example/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.example.jetpack_compose_example
 
+import android.graphics.drawable.Icon
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -14,13 +17,32 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.FavoriteBorder
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.semantics.Role.Companion.Image
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.jetpack_compose_example.ui.theme.JetpackcomposeexampleTheme
@@ -29,43 +51,59 @@ import com.example.jetpack_compose_example.ui.theme.JetpackcomposeexampleTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
-            JetpackcomposeexampleTheme {
-                // A surface container using the 'background' color from the theme
-                Surface {
-                    Box(
-                        modifier = Modifier.background(color = Color.Green)
-                            .fillMaxWidth()
-                            .height(200.dp),
+            var isFavorite by rememberSaveable {
+                mutableStateOf(false)
+            }
 
-                    ) {
-                        Text("Hello")
-                        Box(
-                            modifier = Modifier.fillMaxSize()
-                                .padding(16.dp),
+            ImageCard(
+                modifier = Modifier
+                    .fillMaxWidth(0.5f)
+                    .padding(16.dp),
+                isFavorite = isFavorite,
+            ) { favorite ->
+                isFavorite = favorite
+            }
+        }
+    }
+}
+@Composable
+fun ImageCard(
+    modifier: Modifier = Modifier,
+    isFavorite: Boolean, //외부에서 할당가능하도록
+    onTabFavorite: (Boolean) -> Unit,
+){
+    Card(
+        modifier = Modifier,
+        shape = RoundedCornerShape(8.dp),
+        elevation = CardDefaults.cardElevation(5.dp),
+    ) {
+        Box(
+            modifier = Modifier
+                .height(200.dp)
+        ){
+            Image(painter = painterResource(id = R.drawable.son),
+                contentDescription = "son",
+                contentScale = ContentScale.Crop,
+            )
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.TopEnd,
+            ){
+                IconButton(onClick = {
 
-                            contentAlignment = Alignment.BottomEnd,
-                        ){
-                            Text("123412~~~~~~!!!")
-                        }
-
-                    }
-
+                    onTabFavorite.invoke(!isFavorite)
+                    //Favorite -> 상수이기 때문에 내부에서 변경 처리 불가
+                    //따라서 탭 되었을때 외부에서 값을 받아옴
+                }) {
+                    androidx.compose.material3.Icon(
+                        imageVector = if (isFavorite) Icons.Default.Favorite else Icons.Default.FavoriteBorder,
+                        contentDescription = "favorite",
+                        tint = Color.White,
+                    )
                 }
             }
         }
     }
 }
-
-@Composable
-fun Greeting(name : String){
-    Text(text = "Hello $name!")
-}
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview(){
-    JetpackcomposeexampleTheme() {
-        Greeting("이종민")
-    }
-}
-
